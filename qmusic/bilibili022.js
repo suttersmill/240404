9909c4fe0d2a449d344b6da15e4f68f364c7748b
suttersmill <122515530+suttersmill@users.noreply.github.com>
2024-12-08 22:16:16 +0800

    Update bilibili022.js

--- qmusic/bilibili022.js
+++ qmusic/bilibili022.js
@@ -654,7 +654,7 @@ async function getMusicComments(musicItem) {
 }
 
 module.exports = {
-  platform: "bilibili",
+  platform: "bili022",
   appVersion: ">=0.0",
   version: "0.2.2",
   author: "猫头猫",
